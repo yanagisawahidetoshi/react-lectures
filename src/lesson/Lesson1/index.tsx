af19484623f7e77601d0ab00b610dbfe7edf2f7d
hamano <soichiro.hamano@informatix.co.jp>
2025-04-11 09:28:24 +0900

    課題1の提出

--- src/lesson/Lesson1/index.tsx
+++ src/lesson/Lesson1/index.tsx
@@ -14,29 +14,34 @@ export default function Lesson1() {
 
   // 1. idが1のtodoを見つける
   const findTodoById1 = () => {
-    console.log('findTodoById1');
+    // 仕様が明確ではないので複数一致する場合も考慮
+    console.log(todos.filter(v => v.id === 1))
   };
 
   // 2. newTodoをtodosに追加する
   const addTodo = () => {
     const newTodo = { id: 6, title: 'Todo 6', completed: false };
-
-    console.log('addTodo');
+    todos.push(newTodo)  // 課題の指示は元のリストを更新せよというものに読めるのでこのようにしている
+    console.log(todos);
   };
 
   // 3. completed: true のtodoのみ表示する
   const filterCompletedTodos = () => {
-    console.log('filterCompletedTodos');
+    console.log(todos.filter(v => v.completed))
   };
 
   // 4. idが1のtodoのtitleをUpdated Todo 1に変更する
   const editTodoById1 = () => {
-    console.log('editTodoById1');
+    todos.filter(v => v.id === 1).forEach(v => v.title = 'Updated Todo 1')
+    console.log(todos)
   };
 
   // 5. idが1のtodoを削除する
   const deleteTodoById1 = () => {
-    console.log('deleteTodoById1');
+    todos.forEach((v, k) => {
+      if (v.id === 1) delete todos[k]
+    })
+    console.log(todos)
   };
 
   example();
