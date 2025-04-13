32eb2ab1a3987a3264eae0d286e2cb71030891b6
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2025-04-13 15:09:45 +0900

    lesson1のサンプル解答追加

--- src/lesson/Lesson1/index.tsx
+++ src/lesson/Lesson1/index.tsx
@@ -15,28 +15,39 @@ export default function Lesson1() {
   // 1. idが1のtodoを見つける
   const findTodoById1 = () => {
     console.log('findTodoById1');
+    const foundTodo = todos.find((todo) => todo.id === 1);
+    console.log(foundTodo);
   };
 
   // 2. newTodoをtodosに追加する
   const addTodo = () => {
     const newTodo = { id: 6, title: 'Todo 6', completed: false };
-
     console.log('addTodo');
+    const updatedTodos = [...todos, newTodo];
+    console.log(updatedTodos);
   };
 
   // 3. completed: true のtodoのみ表示する
   const filterCompletedTodos = () => {
     console.log('filterCompletedTodos');
+    const completedTodos = todos.filter((todo) => todo.completed === true);
+    console.log(completedTodos);
   };
 
   // 4. idが1のtodoのtitleをUpdated Todo 1に変更する
   const editTodoById1 = () => {
     console.log('editTodoById1');
+    const updatedTodos = todos.map((todo) =>
+      todo.id === 1 ? { ...todo, title: 'Updated Todo 1' } : todo
+    );
+    console.log(updatedTodos);
   };
 
   // 5. idが1のtodoを削除する
   const deleteTodoById1 = () => {
     console.log('deleteTodoById1');
+    const updatedTodos = todos.filter((todo) => todo.id !== 1);
+    console.log(updatedTodos);
   };
 
   example();
