1c3a1467ee3d4cc7da16882663b90f8aad935a93
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2025-04-13 21:26:11 +0900

    モックデータ追加

--- src/pages/ToDos/index.tsx
+++ src/pages/ToDos/index.tsx
@@ -1,10 +1,35 @@
 export const ToDos = () => {
   const todos = [
-    { id: 1, title: 'Todo 1', completed: false },
-    { id: 2, title: 'Todo 2', completed: true },
-    { id: 3, title: 'Todo 3', completed: false },
-    { id: 4, title: 'Todo 4', completed: true },
-    { id: 5, title: 'Todo 5', completed: false },
+    {
+      id: 1,
+      title: 'Todo 1',
+      isCompleted: false,
+      createdAt: '2025-04-10 08:45',
+    },
+    {
+      id: 2,
+      title: 'Todo 2',
+      isCompleted: true,
+      createdAt: '2025-04-12 15:20',
+    },
+    {
+      id: 3,
+      title: 'Todo 3',
+      isCompleted: false,
+      createdAt: '2025-04-09 11:10',
+    },
+    {
+      id: 4,
+      title: 'Todo 4',
+      isCompleted: true,
+      createdAt: '2025-04-11 17:30',
+    },
+    {
+      id: 5,
+      title: 'Todo 5',
+      isCompleted: false,
+      createdAt: '2025-04-13 09:55',
+    },
   ];
 
   console.log(todos);
