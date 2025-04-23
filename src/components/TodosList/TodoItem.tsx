da16aacd3a6e809e327e6d14839ee471d9d02e43
hamano <soichiro.hamano@informatix.co.jp>
2025-04-23 12:35:53 +0900

    キャンセル後に再度編集ボタンを押すと、キャンセル前の編集状態になる不具合を修正

--- src/components/TodosList/TodoItem.tsx
+++ src/components/TodosList/TodoItem.tsx
@@ -58,6 +58,7 @@ const TodoItem: React.FC<{
             <button
               className="cancel"
               onClick={() => {
+                setTitle(todoItem.title);
                 todoItemHandlers.cancelEdit(todoItem.id);
               }}
             >
